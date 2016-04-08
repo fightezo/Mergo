3000e658d87a48e2791b957895f135115da29e5e
Douglas Wong <fight320@hotmail.com>
2016-04-08 16:28:37 -0700

    Foundation
    Foundation Unity files and Cardboard SDK integrated; added a sandbox
    scene for testing purposes.

---
+++ GAJ16/Assets/Scripts/ReflectionTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReflectionTest : MonoBehaviour
+{
+    CardboardHead head;
+    float delay;
+    // Use this for initialization
+    GameObject user;
+    void Awake()
+    {
+        head = Camera.main.GetComponent<StereoController>().Head;
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+        if (isLookedAt == true)
+        {
+            Debug.Log("Looked at");
+        }
+    }
+}
